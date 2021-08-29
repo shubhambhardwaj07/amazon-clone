f3784baa1a30b75e7fc64a95bc7450844f237ff2
Shubham <shubham998845@gmail.com>
2021-08-29 13:03:48 +0530

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img className="home__image" src="Banner-2.jpg" alt="banner" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBWNjtGNh4ayDrWe2o0l7Vs3Z1nCgS-3Y",
+  authDomain: "clone-e37b7.firebaseapp.com",
+  projectId: "clone-e37b7",
+  storageBucket: "clone-e37b7.appspot.com",
+  messagingSenderId: "918889759586",
+  appId: "1:918889759586:web:8a5824c2d0b65c5ab3a23b"
+};
